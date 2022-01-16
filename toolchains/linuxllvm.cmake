5a11707b98539a15bc654f9c49cf13c3ea5d6e99
Kelvium <ufh8945@googlemail.com>
2022-01-16 17:05:26 +0300

    Add Linux LLVM toolchain
    It uses ccache, clang, lld, somewhat hardened, and some other llvm things
    
    Signed-off-by: Kelvium <ufh8945@googlemail.com>

---
+++ toolchains/linuxllvm.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_C_COMPILER "clang")
+set(CMAKE_CXX_COMPILER "clang++")
+set(CMAKE_C_COMPILER_LAUNCHER "ccache")
+set(CMAKE_CXX_COMPILER_LAUNCHER "ccache")
+
+set(CMAKE_C_COMPILER_TARGET x86_64-pc-linux-gnu)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-pc-linux-gnu)
+
+# TODO: FORTIFY_SOURCE?
+set(COMMON_CFLAGS "-fPIC -flto=thin -fstack-protector-strong")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_CFLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_CFLAGS}")
+
+# TODO: LLVM libunwind
+set(COMMON_LDFLAGS "-fuse-ld=lld -rtlib=compiler-rt -Wl,--as-needed -Wl,-z,relro,-z,now -pie -fPIC")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "${COMMON_LDFLAGS}")
+set(CMAKE_MODULE_LINKER_FLAGS_INIT "${COMMON_LDFLAGS}")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "${COMMON_LDFLAGS}")
